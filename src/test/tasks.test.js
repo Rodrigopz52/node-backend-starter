3a535c2d528232d1071dc9a227bdca21efe3e1f7
Rodrigo Federico Isidro Paz <149693028+Rodrigopz52@users.noreply.github.com>
2026-01-06 18:27:31 -0300

    test: add health endpoint test

---
+++ src/test/tasks.test.js
@@ -0,0 +1,11 @@
+const request = require ("supertest");
+const app = require ("../app")
+
+describe ("GET /health", () => {
+    it ("should return 200", async () => {
+  const response = await request(app).get("/health");
+
+  expect(response.status).toBe(200);
+  expect(response.body).toEqual({ status: "OK" });
+    })
+})
